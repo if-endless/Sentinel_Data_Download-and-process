e8d998f5cdb4f38805c72010c6e1008a712346a1
longchaohuo <60539729+GISer-longchaohuo@users.noreply.github.com>
2022-08-29 22:50:42 +0800

    Add files via upload

---
+++ sentinel_5P data download.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+'''
+@author: LongChaohuo
+@contact: 460958592@qq.com
+@software: PyCharm
+@file: Sentinel5P_Download
+@time: 2022/08/29 晚上 22:37
+
+'''
+# 导入相应的模块
+from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
+from datetime import date
+import time
+import datetime
+import os
+import pathlib
+
+# sentinel-2数据下载函数
+def sentinel_3_data(user_name,user_password,sentinel_url,start_date,end_date):
+    '''
+    :param user_name: 哥白尼数据访问中心账号
+    :param user_password:哥白尼数据访问中心账号密码
+    :param sentinel_url: 哥白尼数据访问中心URL
+    :param start_date: 数据搜索起始时间
+    :param end_date: 数据搜索结束时间
+    :param platformname: 卫星（sentinel卫星）
+    :param max_cloud: 最大云量
+    :param download_dir 数据下载到本地地址
+    '''
+    # 获取文件下全部json文件(绝对路径)
+    # 兴趣区（https://geojson.io/）
+    while True:
+        areas = list(pathlib.Path(path).glob('*.geojson'))
+        print(areas)
+        #遍历json文件
+        for AOI in areas:
+            # 拼接路径
+            filename = os.path.join(path, AOI)
+            print(f'兴趣区路径为：{filename}')
+            #获取兴趣区名称
+            out_file = filename.split(".geojson")[0]
+            print(f'输出路径为：{out_file}')
+            # 根据文件名创建文件夹
+            if not os.path.exists(out_file):
+                os.makedirs(out_file)
+            download_area = geojson_to_wkt(read_geojson(filename))
+            #  设置连接sentinel数据中心的相关参数
+            sentinel_API = SentinelAPI(user_name,user_password,sentinel_url)
+            sentinel_products = sentinel_API.query(
+                    area=download_area,#下载兴趣区域
+                    date=(start_date,end_date),#时间间隔
+                    # platformname= platformname, #卫星
+                    producttype = producttype,#数据等级
+                    # cloudcoverpercentage = (0, max_cloud) #云量设置
+            )
+            print(f'共有产品{len(sentinel_products)}个')
+
+            #遍历查询列表的每一个数据产品
+            for product in sentinel_products:
+                #根据产品元数据字典
+                product_dict = sentinel_API.get_product_odata(product)
+                # print(product_dict)
+                #获取产品id
+                product_ID = product_dict['id']
+                # 获取产品文件title
+                product_title = product_dict['title']
+                print(f'产品名称为{product_title}')
+                #通过产品id查询产品
+                product_info = sentinel_API.get_product_odata(product_ID)
+                #获取产品的在线及离线状态信息
+                product_online = product_info['Online']
+                #判断产品是否在线
+                if product_online: #在线
+                    print(f'产品为{product_title}在线')
+                    #判断本地是否有完整产品
+                    if not os.path.isfile(out_file + os.sep + product_title + ".zip"):
+                        print(f'本地无 {product_title}.zip 的完整文件')
+                        #通过产品id下载产品
+                        sentinel_API.download(product_ID,directory_path= out_file)
+                else:#产品不在线
+                    print(f'产品为{product_title}不在线')
+                    # 判断本地是否有完整产品
+                    if not os.path.isfile(out_file + os.sep + product_title + ".zip"):
+                        print(f'本地无{product_title}.zip 的完整文件，尝试触发 LongTermArchive 库')
+                        try: # 尝试触发
+                            sentinel_API.download(product_info['id'], directory_path= out_file)
+                            sentinel_API.trigger_offline_retrieval(product_ID)
+                            break  #成功则跳出
+                        except Exception as e:
+                            print(f'[错误]请求失败,休眠 15分钟后重试（当前时间：{datetime.datetime.now()}')
+            # 每隔15min才能提交一次请求
+            time.sleep(60 * 15)
+
+
+if __name__ == '__main__':
+    # 存放数据地址
+    path = r'C:\Users\1\Desktop\思维导图\脚本（杂）\sentinel_data download\research area'
+    # 账号及密码（固定的,全世界都一样）
+    user_name='s5pguest'
+    user_password = "s5pguest"
+    # 哥白尼数据访问5p的URL（与1/2/3不同）
+    sentinel_url = 'https://s5phub.copernicus.eu/dhus'
+    # 时间间隔
+    start_date ='20220628'
+    end_date ='20220728'
+    # 下载Sentinel卫星
+    # platformname = 'Sentinel-5P'
+    # 数据类型
+    '''
+    可选                       
+    L1B_IR_SIR, L1B_IR_UVN, L1B_RA_BD1, L1B_RA_BD2, L1B_RA_BD3, L1B_RA_BD4, L1B_RA_BD5, L1B_RA_BD6, L1B_RA_BD7, 
+    L1B_RA_BD8, L2__AER_AI, L2__AER_LH, L2__CH4, L2__CLOUD_, L2__CO____, L2__HCHO__, L2__NO2___, L2__NP_BD3, L2__NP_BD6,
+    L2__NP_BD7, L2__O3_TCL, L2__O3____, L2__SO2___, AUX_CTMFCT、AUX_CTMANA
+     
+    '''
+    producttype = 'L2__NO2___','L2__O3_TCL'
+    sentinel_3_data(user_name, user_password, sentinel_url, start_date, end_date)
